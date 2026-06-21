1cd46f9fd6c65f1c8c6aacda451248812b656ceb
28-aditya <93944077+28-aditya@users.noreply.github.com>
2026-06-21 18:08:37 +0400

    initialized sql querying for user authentication

---
+++ services/user_service.py
@@ -0,0 +1,48 @@
+from sqlalchemy import select, create_engine, Column, String, Integer, DateTime, Date
+from sqlalchemy.orm import sessionmaker
+from db.models import User
+from passlib.context import CryptContext
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+from db.session import Base
+
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, 
+                       echo=True, 
+                       pool_pre_ping=True
+                       )
+
+SessionLocal = sessionmaker(autocommit=False, 
+                            autoflush=False, 
+                            bind=engine
+                            )
+
+def create_or_get_user(userDict):
+
+    db = SessionLocal()
+    try:
+        user = db.query(User).filter(User.email == userDict["email"]).first()
+        
+        if user:
+            return user
+        
+        new_user = User(
+            email=userDict["email"],
+            name=userDict.get("name"),
+            auth_provider=userDict.get("auth_provider"),
+            created_at = datetime.now(timezone.utc),
+            password_hash = userDict.get("password_hash"),
+            provider_user_id = userDict.get("provider_user_id")
+        )
+        
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+        return new_user
+    finally:
+        db.close()
