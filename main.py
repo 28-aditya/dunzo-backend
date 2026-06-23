bf2b954756fc8a77f302a01dcb85487f9649fce3
28-aditya <93944077+28-aditya@users.noreply.github.com>
2026-06-23 23:40:34 +0400

    added cors middleware

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth.google import router as google_router
 
 from db.session import Base, engine
 from routes.me import router as me_router
 
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = os.getenv("APP_BASE_URL"),
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers = ["*"]
+)
 
 app.include_router(google_router)
 app.include_router(me_router)
