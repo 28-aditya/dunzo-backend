f91bbb446df0b634c83cc8d0920fd257ae8767bd
28-aditya <93944077+28-aditya@users.noreply.github.com>
2026-06-28 16:08:52 +0400

    added email authentication functionality

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from auth.google import router as google_router
-
-from db.session import Base, engine
-from routes import me, notes, tasks, settings
-
 from dotenv import load_dotenv
-
 import os
 
 load_dotenv()
 
+from auth.google     import router as google_router
+from auth.email_auth import router as email_router
+from db.session      import Base, engine
+from routes          import me, notes, tasks, settings, linked_task
+
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = [os.getenv("APP_BASE_URL")],
-    allow_credentials = True,
+    allow_origins=[os.getenv("APP_BASE_URL")],
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers = ["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(google_router)
-
+app.include_router(email_router)
 app.include_router(tasks.router)
 app.include_router(notes.router)
 app.include_router(settings.router)
 app.include_router(me.router)
+app.include_router(linked_task.router)
 
 @app.on_event("startup")
 def startup():
